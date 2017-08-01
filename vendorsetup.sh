1a9eb24adfac804abd2d5beae06ad5b0894e4a36
Marcin Flaumenhaft <poncz3k@gmail.com>
2017-08-01 14:07:45 +0100

    vendor_aicp: fix git user name
    Change-Id: I3bafff57ae08301576d4be9909c3ccdcf3c59de9

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s  https://raw.githubusercontent.com/N3xtbit/vendor_jenkins/n7.1/aicp-{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,nightly}-targets | sed -e 's/#.*$//' | grep n7.1 | awk {'print $2'})
+for combo in $(curl -s  https://raw.githubusercontent.com/N3xtBit/vendor_jenkins/n7.1/aicp-{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,nightly}-targets | sed -e 's/#.*$//' | grep n7.1 | awk {'print $2'})
 do
     add_lunch_combo $combo
 done
